94c5a88b480643762d5af06ec30bf41d22838b55
Jonny Pickard <jonathandpickard@gmail.com>
2016-12-06 17:10:56 +0000

    Initial commit

---
+++ notes.md
@@ -0,0 +1,4 @@
+1. Compile from typescript to JS
+- `$ tsc tsfile.ts --target ES5`
+2. Automatically compile when file changed
+- `$ tsc *.ts --watch --target ES5`
---
+++ tstutorial.ts
@@ -0,0 +1,9 @@
+// Declaring types
+// - this is optional
+
+var myName: string = "Jonny";
+var myAge: number = 24;
+var canVote: boolean = true;
+var anything: any = "";
+
+anything = 7;
